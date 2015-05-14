af07fa97f408404688be4c2215e10525fbd5394a
Yujun Liu <yujun.liu@vanderbilt.edu>
2015-05-14 18:13:47 -0400

    added 2 more questions

--- string/twosum2.py
+++ string/twosum2.py
@@ -5,19 +5,14 @@ Similar to Question [1. Two Sum], except that the input array is already sorted 
 def twoSum(lst,target):
     i = 0
     j = len(lst) - 1
-    result = []
-
     while i < j:
-        print i,j
         sumOfTwo = lst[i] + lst[j]
         if sumOfTwo < target:
             i += 1
         elif sumOfTwo > target:
             j -= 1
         else:
-            result.append(i+1)
-            result.append(j+1)
-            return result
+            return [i+1,j+1]
 
 def main():
     l = [2,7,11,15]
