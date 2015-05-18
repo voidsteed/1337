7bc03102e801df1969d931a118b60b37f933beb2
Yujun Liu <yujun.liu@vanderbilt.edu>
2015-05-17 21:26:27 -0400

    new practice

---
+++ dataStructure/histogram.py
@@ -0,0 +1,36 @@
+"""
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, 
+find the area of largest rectangle in the histogram.
+"""
+
+def largestRectangleArea(height):
+    result = 0
+    stack = []
+
+    # Has to reach height's length
+    for i in range(0,len(height)+1):
+        curHeight = 0
+        
+        if len(height) == i:
+            curHeight = 0
+        else:
+            curHeight = height[i]
+        # check if the stack is empty
+        while stack != []:
+            if curHeight < height[stack[-1]]:
+                heightId = stack.pop(-1)
+                prev = -1
+                if stack != []:
+                    prev = stack[-1]
+                result = max(result, (i - prev - 1) * height[heightId])
+            else:
+                break
+
+        stack.append(i)
+
+    return result
+
+def main():
+    l = [2,1,5,6,2,3]
+    print largestRectangleArea(l)
+main()
